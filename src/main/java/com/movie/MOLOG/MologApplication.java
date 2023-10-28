b71877632c2b0b21679ba3bc2d1de2caf8da3e14
minkyung <minkyung7365@gmail.com>
2023-10-28 18:47:46 +0900

    initial commit

---
+++ src/main/java/com/movie/MOLOG/MologApplication.java
@@ -0,0 +1,13 @@
+package com.movie.MOLOG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MologApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MologApplication.class, args);
+	}
+
+}
